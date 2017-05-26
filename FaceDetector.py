c077f3e531bdbb8ec11e0a0c5399d83ccf31810e
nirvash <nirvash@gmail.com>
2017-05-26 11:54:45 +0900

    選択範囲を正方形に自動修正

--- FaceDetector.py
+++ FaceDetector.py
@@ -21,8 +21,8 @@ class FaceDetector:
             gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
             gray = cv2.equalizeHist(gray)
             (faces, neighbors, weights) = cascade.detectMultiScale3(gray, scaleFactor=scale, minNeighbors=neighbor, minSize=(24, 24), outputRejectLevels=True)
-            color = (255, 0, 0) #RGB
-            CV_AA = 16
+            color = (0, 200, 0) #RGB
+            # CV_AA = 16
 
             if len(faces) > 500:
                 print "too many face detected {0}".format(len(faces))
@@ -30,10 +30,10 @@ class FaceDetector:
                 for i, rect in enumerate(faces):
                     cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=4)
                     pos = (rect[0], rect[1] - 5)
-                    text = "({0} * {1:04.2f})".format(neighbors[i, 0], weights[i, 0])
-                    if i < 5:
-                        print text
-                    cv2.putText(img, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 2, CV_AA)
+                    # text = "({0} * {1:04.2f})".format(neighbors[i, 0], weights[i, 0])
+                    # if i < 5:
+                    #    print text
+                    # cv2.putText(img, text, pos, cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 2, CV_AA)
         except:
             traceback.print_exc()
 
@@ -47,16 +47,19 @@ class FaceDetector:
             for i, rect in enumerate(rects):
                 if rect.width() < 24 or rect.height() < 24:
                     continue
-                output = ''
-                cropped = img[rect.y():rect.y()+rect.height(), rect.x():rect.x()+rect.width()]
-                if mirror:
-                    cropped = cv2.flip(cropped, 1) # 1: Y Axis
-                output = "{0}crop_{1}_{2:0>3d}.jpg".format(path, basefilename, i + baseIndex)
-                while os.path.exists(output):
-                    baseIndex += 1
-                    output = "{0}crop_{1}_{2:0>3d}.jpg".format(path, basefilename, i + baseIndex)
 
-                cv2.imwrite(output, cropped)
-                print output
+                self.cropImpl(baseIndex, basefilename, i, img, mirror, path, rect)
         except:
             traceback.print_exc()
+
+    def cropImpl(self, baseIndex, basefilename, i, img, mirror, path, rect):
+        output = ''
+        cropped = img[rect.y():rect.y() + rect.height(), rect.x():rect.x() + rect.width()]
+        if mirror:
+            cropped = cv2.flip(cropped, 1)  # 1: Y Axis
+        output = "{0}crop_{1}_{2:0>3d}.jpg".format(path, basefilename, i + baseIndex)
+        while os.path.exists(output):
+            baseIndex += 1
+            output = "{0}crop_{1}_{2:0>3d}.jpg".format(path, basefilename, i + baseIndex)
+        cv2.imwrite(output, cropped)
+        print output
--- RubberBandGraphicsView.py
+++ RubberBandGraphicsView.py
@@ -15,14 +15,35 @@ class RubberBandGraphicsView(QGraphicsView):
 
     def __init__(self, parent = None):
         super(QGraphicsView, self).__init__(parent)
+        self.pressPos = QPoint(0, 0)
+
+    def mousePressEvent(self, event):
+        super(RubberBandGraphicsView, self).mousePressEvent(event)
+        self.pressPos = event.pos()
 
     def mouseReleaseEvent(self, event):
         rect = self.rubberBandRect()
 #        QGraphicsView.mouseReleaseEvent(self, event)
         super(RubberBandGraphicsView, self).mouseReleaseEvent(event)
         if not rect.isNull():
+            rect = self.normalizeRect(rect, self.pressPos, event.pos())
             self.rubberBandSelected.emit(rect)
 
         if event.button() == QtCore.Qt.RightButton:
             self.clicked.emit(event.pos())
+
+    def normalizeRect(self, rect, pos1, pos2):
+        diff = rect.width() - rect.height()
+        if diff < 0: # 幅を広げる
+            diff = abs(diff)
+            if pos1.x() > pos2.x(): # 始点が右
+                rect.setX(rect.x() - diff)
+            else:
+                rect.setWidth(rect.width() + diff)
+        else: # 高さを広げる
+            if pos1.y() > pos2.y(): # 始点が上
+                rect.setY(rect.y() - diff)
+            else:
+                rect.setHeight(rect.height() + diff)
+        return rect
 
